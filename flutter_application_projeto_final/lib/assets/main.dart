76096a32a459be52653fb84a4e55a5dd185ebb6a
carol <eriushirobira@hotmail.com>
2021-11-29 00:49:57 -0300

    Implementing "usuarios" collection

--- flutter_application_projeto_final/lib/assets/main.dart
+++ flutter_application_projeto_final/lib/assets/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -113,12 +114,7 @@ class _LoginPageState extends State<LoginPage> {
                 child: Text('Login'),
                 onPressed: () {
                   setState(() {
-                    isLoading = true;
-                    // var obj = Infos(
-                    //   username.text,
-                    // );
-
-                    // Navigator.pushNamed(context, 't2', arguments: obj);                    
+                    isLoading = true;                
                   });
                   login(txtEmail.text, txtSenha.text);
                 },
@@ -178,7 +174,6 @@ class _LoginPageState extends State<LoginPage> {
       FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: senha)
           .then((value) {
-        // Navigator.pushReplacementNamed(context, '/t2');
         Navigator.of(context)
           .push(MaterialPageRoute(builder: (context) => TelaPrincipal()));
       }).catchError((erro) {
@@ -192,7 +187,6 @@ class _LoginPageState extends State<LoginPage> {
           exibirMensagem('ERRO: ${erro.message}.');
         }
       });
-
   }
 
   void exibirMensagem(msg) {
@@ -215,140 +209,85 @@ class Register extends StatefulWidget {
 enum Sexo { masculino, feminino }
 
 class _RegisterState extends State<Register> {
-  Sexo? _character = Sexo.masculino;
-  var _checkbox = false;
+  late CollectionReference usuarios;
   var txtNome = TextEditingController();
   var txtEmail = TextEditingController();
   var txtSenha = TextEditingController();
+  var txtCPF = TextEditingController();
+  var txtTelefone = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown.shade100,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [                  
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(height: 20),
-                    Text(
-                      "Cadastre-se!",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      "Preencha os dados abaixo para criar uma conta:",
-                      style: TextStyle(
-                        fontSize: 17,
-                      ),
-                    ),
-                    SizedBox(height: 20)
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Column(
-                    children: <Widget>[
-                      makeInput(
-                          controllerword: txtNome,
-                          label: "Nome de Usuário:"),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('Sexo:',
-                            style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.normal,
-                            )),
-                      ),
-                      ListTile(
-                        title: const Text('Masculino'),
-                        leading: Radio<Sexo>(
-                          value: Sexo.masculino,
-                          groupValue: _character,
-                          onChanged: (Sexo? value) {
-                            setState(() {
-                              _character = value;
-                            });
-                          },
-                        ),
-                      ),
-                      ListTile(
-                        title: const Text('Feminino'),
-                        leading: Radio<Sexo>(
-                          value: Sexo.feminino,
-                          groupValue: _character,
-                          onChanged: (Sexo? value) {
-                            setState(() {
-                              _character = value;
-                            });
-                          },
-                        ),
-                      ),
-                      makeInput(
-                        controllerword: txtEmail,
-                        label: "E-mail:", 
-                        obsureText: false
-                      ),
-                      makeInput(
-                        controllerword: txtSenha,
-                        label: "Senha:", 
-                        obsureText: false
-                      ),
-                      Row(
-                        children: [
-                          Checkbox(
-                            value: _checkbox,
-                            onChanged: (value) {
-                              setState(() {
-                                _checkbox = !_checkbox;
-                              });
-                            },
-                          ),
-                          Text(
-                              'Concordo com os termos de uso deste aplicativo.'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 30),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    child: ElevatedButton(
-                        child: Text('Registrar'),
-                        onPressed: () {
-                          criarConta(
-                            txtNome.text,
-                            txtEmail.text,
-                            txtSenha.text,
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.grey.shade800,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 30, vertical: 20),
-                        )),
-                  ),
-                ),                                 
-              ],
+      body: Container(     
+        padding: EdgeInsets.all(50.0),
+        child: Column(
+          children: [                  
+            Text(
+              "Cadastre-se!",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
+            SizedBox(height: 20),
+            Text(
+              "Preencha os dados abaixo para criar uma conta:",
+              style: TextStyle(
+                fontSize: 17,
+              ),
+            ),
+            SizedBox(height: 40),
+            makeInput(
+              controllerword: txtNome,
+              label: "Nome de Usuário:"
+            ),
+            makeInput(
+              controllerword: txtEmail,
+              label: "E-mail:", 
+              obsureText: false
+            ),
+            makeInput(
+              controllerword: txtSenha,
+              label: "Senha:", 
+              obsureText: false
+            ),
+            makeInput(
+              controllerword: txtCPF,
+              label: "CPF:", 
+              obsureText: false
+            ),
+            makeInput(
+              controllerword: txtTelefone,
+              label: "Telefone:", 
+              obsureText: false
+            ),
+            ElevatedButton(
+              child: Text('Registrar'),
+              onPressed: () {
+                criarConta(
+                  txtNome.text,
+                  txtEmail.text,
+                  txtSenha.text,
+                  txtCPF.text,
+                  txtTelefone.text,
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey.shade800,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30, vertical: 20),
+              )
+            ),
+          ]
         ),
-      ),
+      ),   
     );
   }
+  
 
-  void criarConta(nome, email, senha) {
+  void criarConta(nome, email, senha, cpf, telefone) {
 
     FirebaseAuth.instance
         .createUserWithEmailAndPassword(email: email, password: senha)
@@ -364,7 +303,13 @@ class _RegisterState extends State<Register> {
             exibirMensagem('ERRO: ${erro.message}');
           }
         });
-
+    FirebaseFirestore.instance.collection('usuarios').add({
+        'nome': txtNome.text,
+        'CPF': txtCPF.text,
+        'telefone': txtTelefone.text,
+        'email': txtEmail.text,
+        'senha': txtSenha.text
+    });
   }
 
   void exibirMensagem(msg) {
